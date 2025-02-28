8510c1855e2047ba87c1afb092c5f7096bad4caa
MatthewBDreier <149102414+MatthewBDreier@users.noreply.github.com>
2025-02-28 10:36:16 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/pricing-slider/',
+  base: '/allied-installer-price-picker/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
